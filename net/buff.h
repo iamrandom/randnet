a4b9d30759773fe89697c4a1a36d01a38095f1a5
iamrandom <huangxiaobin_sea@163.com>
2016-02-02 19:37:46 +0800

    modified:   net/buff.h

--- net/buff.h
+++ net/buff.h
@@ -28,25 +28,40 @@ enum EnByteSize
 
 
 struct recv_buff;
-struct recv_buff*	recv_buff_create(enum EnByteSize en_byte, uint16_t pool_cnt, struct buff_pool* pool);
-void				recv_buff_release(struct recv_buff* rbuff);
-size_t				recv_buff_prepare(struct recv_buff* rbuff, void** pdata);
-int					recv_buff_consume(struct recv_buff* rbuff, size_t usize);
-int					recv_buff_read(struct recv_buff* rbuff, void* buff, int usize);
+struct recv_buff*	
+recv_buff_create(enum EnByteSize en_byte, uint16_t pool_cnt, struct buff_pool* pool);
+void				
+recv_buff_release(struct recv_buff* rbuff);
+size_t				
+recv_buff_prepare(struct recv_buff* rbuff, void** pdata);
+int					
+recv_buff_consume(struct recv_buff* rbuff, size_t usize);
+int					
+recv_buff_read(struct recv_buff* rbuff, void* buff, int usize);
 
 struct send_buff;
-struct send_buff*	send_buff_create(enum EnByteSize en_byte, uint16_t pool_cnt,  struct buff_pool* pool);
-void				send_buff_release(struct send_buff* sbuff);
-size_t				send_buff_prepare(struct send_buff* sbuff, void** pdata);
-void				send_buff_consume(struct send_buff* sbuff, size_t usize);
-int					send_buff_write(struct send_buff* sbuff, const void* pdata, int size);
+struct send_buff*	
+send_buff_create(enum EnByteSize en_byte, uint16_t pool_cnt,  struct buff_pool* pool);
+void				
+send_buff_release(struct send_buff* sbuff);
+size_t				
+send_buff_prepare(struct send_buff* sbuff, void** pdata);
+void				
+send_buff_consume(struct send_buff* sbuff, size_t usize);
+int					
+send_buff_write(struct send_buff* sbuff, const void* pdata, int size);
 
 struct msg_buff;
-struct msg_buff*	msg_buff_create(size_t msg_type_size, uint16_t buffer_size, uint16_t pool_cnt, struct buff_pool* pool);
-void				msg_buff_release(struct msg_buff* mbuff);
-int					msg_buff_write(struct msg_buff* mbuff, const void* msg);
-int					msg_buff_read(struct msg_buff* mbuff, void* msgs, size_t n);
-unsigned int		msg_buff_size(struct msg_buff* mbuff);
+struct msg_buff*	
+msg_buff_create(size_t msg_type_size, uint16_t buffer_size, uint16_t pool_cnt, struct buff_pool* pool);
+void				
+msg_buff_release(struct msg_buff* mbuff);
+int					
+msg_buff_write(struct msg_buff* mbuff, const void* msg);
+int					
+msg_buff_read(struct msg_buff* mbuff, void* msgs, size_t n);
+unsigned int		
+msg_buff_size(struct msg_buff* mbuff);
 
 
 
